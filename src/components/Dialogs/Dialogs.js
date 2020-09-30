e72da346efb6d2cb1471b5a294e38e374448c0f5
devshum <57689318+devshum@users.noreply.github.com>
2020-09-30 14:50:04 +0300

    reducer

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,13 +2,27 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import { updateNewMessageActionCreator, addMessageActionCreater } from '../../redux/state';
 
 const Dialogs = props => {
- 
 
-    const dialogsElements = props.state.dialogsData.map(dialogObj => <DialogItem name={dialogObj.name} id={dialogObj.id} key={dialogObj.id} /> )
+    const state = props.store.getState().dialogsPage;
+   
+    const dialogsElements = state.dialogsData.map(dialogObj => <DialogItem name={dialogObj.name} id={dialogObj.id} key={dialogObj.id} /> )
 
-    const messagesElements = props.state.messagesData.map(messageObj => <Message text={messageObj.message} id={messageObj.id} key={messageObj.id} /> )
+    const messagesElements = state.messagesData.map(messageObj => <Message text={messageObj.message} id={messageObj.id} key={messageObj.id} /> )
+
+    const onMessageChange = e => {
+        const text = e.target.value;
+
+        props.store.dispatch(updateNewMessageActionCreator(text));
+    };
+
+
+    const onMessageClick = () => {
+        props.store.dispatch( addMessageActionCreater() );
+    };
+
 
     return(
         <div className={classes.dialogs}>
@@ -21,8 +35,15 @@ const Dialogs = props => {
             <div className="messages">
 
                 { messagesElements }
+
+                <textarea placeholder="Enter new message" onChange={onMessageChange} value={props.store.newMessageText}></textarea>
+                <div>
+                    <button onClick={onMessageClick}>Add message</button>
+                </div>
             
             </div>
+
+            
         </div>
     );
 };

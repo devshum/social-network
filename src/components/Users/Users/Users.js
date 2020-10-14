26d394cd4bca10ed0a07687488ae56c62dfa7646
devshum <57689318+devshum@users.noreply.github.com>
2020-10-14 19:46:35 +0300

    preloader, loader-gif, isFetching indication

---
+++ src/components/Users/Users/Users.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Users.module.css';
+import defaultAvatar from '../../../assets/default-avatar.jpg';
+
+
+const Users = props => {
+    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    const pages = [];
+    for(let i = 1; i <= pagesCount; i++) { pages.push(i) }
+
+    return <div className={styles.userPage}>
+                <h3>Users</h3>
+                <div>
+                    {pages.map(page => <span className={props.currentPage === page && styles.selectedPage}
+                                            onClick={e => { props.onPageChanged(page) }}>{page}</span>)}
+                </div>
+                {
+                    props.users.map(user => <div key={user.id} className={styles.userLabel}>
+                            <div className={styles.user}>
+                                <img src={ user.photos.small !== null ? user.photos.small : defaultAvatar} />
+                                
+                                <button className={ user.followed ? styles.followedBtn 
+                                                                : styles.unfollowedBtn } 
+
+                                        onClick={() => props.toggleFollow(user.id)}>{ user.followed ? 'Unfollow' 
+                                                                                                        : 'Follow'}
+                                </button>
+                            </div>
+                            <div className={styles.userInfo}>
+                                <div>
+                                    <div className={styles.userTextBold}>{user.name}</div>
+                                    <div>{user.status}</div>
+                                </div>
+                                <div>
+                                    <div className={styles.userTextBold}>{'user.location.country'}</div>
+                                    <div className={styles.userTextBold}>{'user.location.city'}</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+                <button onClick={props.getUsers}>Get Users</button>
+            </div> 
+    }
+ 
+
+export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,10 +1,45 @@
-import Users from "./Users";
+import React from 'react';
+import * as axios from 'axios';
+import Users from './Users/Users';
+
 import {connect} from "react-redux";
 
 import { toggleFollowActionCreator,
          setUsersActionCreator,
          setCurrentPageActionCreator,
          setTotalUsersCountActionCreator } from "../../redux/users-reducer";
+
+
+class UsersAPIComponent extends React.Component {
+        componentDidMount() {
+            axios
+                .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+                .then(response => {
+                this.props.setUsers(response.data.items);
+                // this.props.setTotalUsersCount(response.data.totalCount);
+            });  
+        }
+        
+        onPageChanged = page => {
+            this.props.setCurrentPage(page);
+    
+            axios
+                .get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+                .then(response => {
+                this.props.setUsers(response.data.items);
+            });  
+        }
+        
+        render() {
+            return <Users totalUsersCount={this.props.totalUsersCount}
+                            pageSize={this.props.pageSize} 
+                            currentPage={this.props.currentPage} 
+                            users={this.props.users} 
+                            toggleFollow={this.props.toggleFollow} 
+                            onPageChanged={this.onPageChanged} 
+                            getUsers={this.props.getUsers}/>
+        }
+};
 
 let mapStateToProps = state => ({ 
     users: state.usersPage.usersData ,
@@ -20,4 +55,4 @@ let mapDispatchToProps = dispatch => ({
     setTotalUsersCount: totalCount => dispatch(setTotalUsersCountActionCreator(totalCount))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
